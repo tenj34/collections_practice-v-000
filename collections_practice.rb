4263f1d227239b646fd8c6be1ed185205b229058
Juan Tenezaca <tenj34@gmail.com>
2018-07-21 20:06:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,8 +39,11 @@ def sum_array (array)
 end
 
 def add_s (array)
-
-
-
-
+  array.collect.with_index do |word, index|
+    if index == 1
+      word
+    else
+      word + "s"
+    end
+  end
 end
