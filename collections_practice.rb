bab023293e3cab4342be337e41bc8cd2952c840b
Juan Tenezaca <tenj34@gmail.com>
2018-07-21 20:07:50 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(num)
+  num.sort do |a,b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(num)
+  num.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(num)
+  num.sort_by(&:length)
+end
+
+def swap_elements(num)
+  num[1], num[2] = num[2],num[1]
+  num
+end
+
+def reverse_array(num)
+  num.reverse!
+end
+
+def kesha_maker(num)
+  num.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
+
+def find_a(array)
+  array.select {|words| words.start_with?("a")}
+end
+
+def sum_array (array)
+  array.inject(0) {|sum,i| sum + i}
+end
+
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1
+      word
+    else
+      word + "s"
+    end
+  end
+end
